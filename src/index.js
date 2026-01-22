92d19914f6ab86ec4688dd98367ffd2316b79f3f
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-01-22 14:53:35 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // 이 라인이 있는지 확인
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// unregister()를 register()로 변경하세요!
+serviceWorkerRegistration.register();
