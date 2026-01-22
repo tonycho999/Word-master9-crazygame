c402f46a4fd55bc5f420904d138311828024fe88
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-01-22 15:05:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // 추가
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,5 +10,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// 이 부분을 반드시 register()로 바꿔야 설치가 됩니다!
+// unregister()에서 register()로 반드시 변경해야 설치가 활성화됩니다.
 serviceWorkerRegistration.register();
