af9b12462d6f2fa08cb3444c6d9d31cc5d22469c
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-01-22 12:06:34 +0800

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,14 @@
-// public/service-worker.js
-const CACHE_NAME = 'word-scramble-v1';
+const CACHE_NAME = 'word-game-v1';
+const urlsToCache = ['/', '/index.html'];
 
-// 설치 시 리소스 캐싱 (오프라인 지원)
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['/', '/index.html', '/manifest.json']);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
