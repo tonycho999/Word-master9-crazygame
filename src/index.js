fb8d533cefa1210ae3fe6261690b0efe9fd64f91
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-02-08 19:31:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // ★ 스타일(Tailwind) 적용을 위해 필수
 import App from './App';
-// [1] import 추가
+
+// ★ PWA 기능(앱 설치, 오프라인) 활성화를 위해 필수
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+// ★ 검색 최적화(SEO)를 위해 필수
 import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* [2] App을 HelmetProvider로 감싸줍니다 */}
+    {/* HelmetProvider로 감싸야 SEO 기능이 작동합니다 */}
     <HelmetProvider>
       <App />
     </HelmetProvider>
   </React.StrictMode>
 );
+
+// ★ PWA 서비스 워커 등록 (이게 없으면 '앱 설치' 버튼이 안 뜹니다)
+serviceWorkerRegistration.register();
