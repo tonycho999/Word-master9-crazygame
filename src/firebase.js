4293561dd63bc9495b5b8aadada52a8d1cbaa7f4
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-02-25 13:17:36 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAA2_ry1326LsvnTAgGOyvrGJf4_kKYgTs",
-  authDomain: "word-master99.firebaseapp.com",
-  projectId: "word-master99",
-  storageBucket: "word-master99.firebasestorage.app",
-  messagingSenderId: "102547168102",
-  appId: "1:102547168102:web:7e0ee3ed76b659336e46a7",
-  measurementId: "G-2JZ2N3G9P2"
+  apiKey: "AIzaSyDQT1f2RnrVtYFG3xTxqNiWeTDicwyQpSg",
+  authDomain: "word-master9-crazygame.firebaseapp.com",
+  projectId: "word-master9-crazygame",
+  storageBucket: "word-master9-crazygame.firebasestorage.app",
+  messagingSenderId: "709273616074",
+  appId: "1:709273616074:web:f80d9d4e0b531a9ffe7e33",
+  measurementId: "G-KZGKWR5PHT"
 };
 
 const app = initializeApp(firebaseConfig);
